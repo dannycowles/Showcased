7f81f45823a191bae903c5d18d455dc50fc1fc65
dannycowles <dannycowless@gmail.com>
2025-02-26 16:40:35 -0800

    Initial commit, Spring Boot setup

---
+++ src/main/java/com/example/showcased/ShowcasedApplication.java
@@ -0,0 +1,13 @@
+package com.example.showcased;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowcasedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShowcasedApplication.class, args);
+    }
+
+}
