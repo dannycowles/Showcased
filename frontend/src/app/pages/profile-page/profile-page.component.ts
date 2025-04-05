3383a6b015f6b296cb957e78f0228edb989028fb
dannycowles <dannycowless@gmail.com>
2025-04-05 13:05:59 -0700

    Reviews added on profile page

--- frontend/src/app/pages/profile-page/profile-page.component.ts
+++ frontend/src/app/pages/profile-page/profile-page.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {ProfileData} from '../../data/profile-data';
 import {ProfileService} from '../../services/profile.service';
+import {UtilsService} from '../../services/utils.service';
 
 @Component({
   selector: 'app-profile-page',
@@ -11,7 +12,8 @@ import {ProfileService} from '../../services/profile.service';
 export class ProfilePageComponent implements OnInit {
   profileData: ProfileData;
 
-  constructor(private profileService: ProfileService) { }
+  constructor(private profileService: ProfileService,
+              public utilsService: UtilsService) { }
 
   async ngOnInit() {
     // Retrieve profile data from backend
