4a87189f147838fb2bf4b37073ef7b403e6676ca
dannycowles <dannycowless@gmail.com>
2025-04-08 16:36:20 -0700

    Work on frontend of password reset

--- frontend/src/app/pages/auth/reset-password-page/reset-password-page.component.ts
+++ frontend/src/app/pages/auth/reset-password-page/reset-password-page.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {AuthenticationService} from '../../../services/auth.service';
 
 @Component({
   selector: 'app-reset-password-page',
@@ -7,9 +8,46 @@ import {Component, OnInit} from '@angular/core';
   standalone: false
 })
 export class ResetPasswordPageComponent implements OnInit {
+  formNumber: number = 1;
+  email: string;
+  otp: string = "";
 
-  constructor() {};
+  constructor(private authService: AuthenticationService) {};
 
   ngOnInit() {};
 
+  async submitEmail() {
+    if (this.email) {
+      // Call backend to generate OTP and send email
+      try {
+        await this.authService.generateOTP(this.email);
+      } catch(error) {
+        console.error(error);
+      }
+      this.formNumber = 2;
+    }
+  }
+
+  onOtpChange(otp: string) {
+    this.otp = otp;
+  }
+
+  async verifyCode() {
+    try {
+      let data = {
+        email: this.email,
+        otp: this.otp
+      };
+      let response: Response = await this.authService.validateOTP(data);
+
+      if (response.ok) {
+        console.log("matched!")
+      } else {
+        console.log(response.text());
+      }
+
+    } catch(error) {
+      console.error(error);
+    }
+  }
 }
