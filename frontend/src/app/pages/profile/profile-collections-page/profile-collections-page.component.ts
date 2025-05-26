bebcc9a4c3a74def3223f0758a97c39796d0b799
dannycowles <dannycowless@gmail.com>
2025-05-25 17:33:15 -0700

    Search & create collection functionality for profile page

--- frontend/src/app/pages/profile/profile-collections-page/profile-collections-page.component.ts
+++ frontend/src/app/pages/profile/profile-collections-page/profile-collections-page.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {ProfileService} from '../../../services/profile.service';
 import {CollectionData} from '../../../data/collection-data';
+import {UtilsService} from '../../../services/utils.service';
 
 @Component({
   selector: 'app-profile-collections-page',
@@ -10,8 +11,12 @@ import {CollectionData} from '../../../data/collection-data';
 })
 export class ProfileCollectionsPageComponent implements OnInit {
   collectionData: CollectionData[];
+  searchCollectionString: string;
+  newCollectionName: string;
+  debouncedSearchCollections: () => void;
 
-  constructor(private profileService: ProfileService) { };
+  constructor(private profileService: ProfileService,
+              public utilsService: UtilsService) { };
 
   async ngOnInit() {
     try {
@@ -19,5 +24,42 @@ export class ProfileCollectionsPageComponent implements OnInit {
     } catch (error) {
       console.error(error);
     }
+
+    this.debouncedSearchCollections = this.utilsService.debounce(() => {
+      this.searchCollections();
+    });
   };
+
+  async searchCollections() {
+    try {
+      this.collectionData = await this.profileService.getCollections(this.searchCollectionString);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async createNewCollection() {
+    const collectionMessage = document.getElementById("collectionError")
+    try {
+      const data = {
+        collectionName: this.newCollectionName
+      };
+
+      const response = await this.profileService.createCollection(data);
+      if (response.ok) {
+        this.collectionData = await this.profileService.getCollections();
+        collectionMessage.innerText = "Collection created!";
+        collectionMessage.style.color = "green";
+      } else {
+        collectionMessage.innerText = "You already have a collection with this name!";
+        collectionMessage.style.color = "red";
+      }
+    } catch(error) {
+      console.error(error);
+    } finally {
+      setTimeout(() => {
+        collectionMessage.innerText = "";
+      }, 3000);
+    }
+  }
 }
