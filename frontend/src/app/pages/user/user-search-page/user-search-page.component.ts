87e8a88f75c7025b6d817e36339572c79d9914ab
Danny Cowles <dannycowless@gmail.com>
2025-07-06 13:30:19 -0700

    Updated user search page

--- frontend/src/app/pages/user/user-search-page/user-search-page.component.ts
+++ frontend/src/app/pages/user/user-search-page/user-search-page.component.ts
@@ -1,6 +1,7 @@
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import {UserSearchData} from '../../../data/user-search-data';
 import {UserService} from '../../../services/user.service';
+import {UtilsService} from '../../../services/utils.service';
 
 @Component({
   selector: 'app-user-search-page',
@@ -8,24 +9,33 @@ import {UserService} from '../../../services/user.service';
   styleUrl: './user-search-page.component.css',
   standalone: false
 })
-export class UserSearchPageComponent implements OnInit {
-  searchString: string;
-  searchResults: UserSearchData[];
+export class UserSearchPageComponent{
+  searchString: string = "";
+  searchResults: UserSearchData[] | null = null;
+  debouncedSearchUsers: () => void;
+  isLoading: boolean = false;
+  hasSearched: boolean = false;
 
-  constructor(private userService: UserService) {};
+  constructor(private userService: UserService,
+              public utilsService: UtilsService) {
+    this.debouncedSearchUsers = this.utilsService.debounce(() => this.searchUsers());
+  };
 
-  ngOnInit() {};
+  async searchUsers() {
+    if (this.searchString.trim().length > 0) {
+      this.isLoading = true;
+      this.hasSearched = true;
 
-  async search() {
-    // As long as the search string is not empty we will call the backend
-    if (this.searchString) {
       try {
         this.searchResults = await this.userService.searchUsers(this.searchString);
-        console.log('searchResults', this.searchResults);
       } catch(error) {
         console.error(error);
+      } finally {
+        this.isLoading = false;
       }
+    } else {
+      this.hasSearched = false;
+      this.searchResults = null;
     }
   }
-
 }
