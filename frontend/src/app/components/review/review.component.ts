30420490d3ba7c4696d0dc05d827817cc961856e
Danny Cowles <dannycowless@gmail.com>
2025-07-15 12:55:58 -0700

    Show page add comment functionality

--- frontend/src/app/components/review/review.component.ts
+++ frontend/src/app/components/review/review.component.ts
@@ -26,6 +26,11 @@ export class ReviewComponent {
   @Input({ required: true }) review: EpisodeReviewData | ShowReviewData;
   @Input({ required: true }) reviewType: ReviewType;
   readonly heartSize = 100;
+  commentText: string = "";
+  showCommentBox: boolean = false;
+
+  readonly maxCommentLength = 1000;
+  commentMessage: string = "";
 
   constructor(
     public utilsService: UtilsService,
@@ -70,6 +75,33 @@ export class ReviewComponent {
       this.review.comments = await handler.getComments();
     } catch (error) {
       console.error(error);
+    }
+  }
+
+  resetCommentBox() {
+    this.showCommentBox = !this.showCommentBox;
+    this.commentText = "";
+  }
+
+  async addComment() {
+    try {
+      const data = {
+        commentText: this.commentText
+      };
+      const newComment = await this.showService.addCommentToShowReview(this.review.id, data);
+
+      // If the comments are already loaded from the backend, push the newComment
+      if (this.review.comments) {
+        this.review.comments.push(newComment);
+      }
+
+      this.review.numComments++;
+      this.showCommentBox = false;
+      this.commentMessage = "Successfully added comment!";
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setTimeout(() => this.commentMessage = "", 3000);
     }
   }
 }
