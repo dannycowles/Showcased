1ae64c6dc14afe76bdb4aa62beed043eca319e12
Danny Cowles <dannycowless@gmail.com>
2025-08-17 16:11:57 -0700

    Modified show page create collection, moved functionality to profile solely

--- frontend/src/app/components/add-to-collection-modal/add-to-collection-modal.component.ts
+++ frontend/src/app/components/add-to-collection-modal/add-to-collection-modal.component.ts
@@ -5,35 +5,36 @@ import {NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
 import {UtilsService} from '../../services/utils.service';
 import {ProfileService} from '../../services/profile.service';
 import {ShowListData} from '../../data/lists/show-list-data';
-import {CreateCollectionDto} from '../../data/dto/create-collection-dto';
 import {AddToCollection} from '../../data/dto/add-to-list-dto';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-add-to-collection-modal',
   imports: [FormsModule],
   templateUrl: './add-to-collection-modal.component.html',
   styleUrl: './add-to-collection-modal.component.css',
-  standalone: true
+  standalone: true,
 })
 export class AddToCollectionModalComponent implements OnInit {
-  @Input({required: true}) show: ShowListData;
-  searchCollectionString: string = "";
+  @Input({ required: true }) show: ShowListData;
+  searchCollectionString: string = '';
   collections: CollectionData[];
-  newCollectionName: string = "";
   selectedCollection: CollectionData | null = null;
 
-  message: string = "";
-  messageColor: string = "";
+  message: string = '';
+  messageColor: string = '';
   isLoading: boolean = false;
-  readonly maxCollectionNameLength = 100;
 
   debouncedSearchCollections: () => void;
 
   constructor(public activeModal: NgbActiveModal,
               public utilsService: UtilsService,
-              private profileService: ProfileService) {
-    this.debouncedSearchCollections = this.utilsService.debounce(() => this.searchCollections());
-  };
+              private profileService: ProfileService,
+              private router: Router) {
+    this.debouncedSearchCollections = this.utilsService.debounce(() =>
+      this.searchCollections(),
+    );
+  }
 
   async ngOnInit() {
     await this.searchCollections();
@@ -42,7 +43,9 @@ export class AddToCollectionModalComponent implements OnInit {
   async searchCollections() {
     this.isLoading = true;
     try {
-      this.collections = await this.profileService.getCollections(this.searchCollectionString);
+      this.collections = await this.profileService.getCollections(
+        this.searchCollectionString,
+      );
     } catch (error) {
       console.error(error);
     } finally {
@@ -50,49 +53,34 @@ export class AddToCollectionModalComponent implements OnInit {
     }
   }
 
-  // async createNewCollection() {
-  //   try {
-  //     const data: CreateCollectionDto = {
-  //       collectionName: this.newCollectionName
-  //     };
-  //
-  //     const response = await this.profileService.createCollection(data);
-  //     if (response.ok) {
-  //       this.message = "Collection created!";
-  //       this.messageColor = "green";
-  //       const newCollection = await response.json();
-  //       this.collections.push(newCollection);
-  //     } else {
-  //       this.message = "You already have a collection with this name!";
-  //       this.messageColor = "red";
-  //     }
-  //   } catch(error) {
-  //     console.error(error);
-  //   } finally {
-  //     setTimeout(() => this.message = "", 3000);
-  //   }
-  // }
+  navigateToCreateNewCollection() {
+    this.activeModal.close();
+    this.router.navigate(['/profile/collections/new']);
+  }
 
   async addToCollection() {
     try {
       const showData: AddToCollection = {
         showId: this.show.showId,
         showTitle: this.show.title,
-        posterPath: this.show.posterPath
+        posterPath: this.show.posterPath,
       };
 
-      const response = await this.profileService.addShowToCollection(this.selectedCollection.id, showData);
+      const response = await this.profileService.addShowToCollection(
+        this.selectedCollection.id,
+        showData,
+      );
       if (response.ok) {
         this.message = `Successfully added to ${this.selectedCollection.name}!`;
-        this.messageColor = "green";
+        this.messageColor = 'green';
       } else {
         this.message = `Show is already in ${this.selectedCollection.name}!`;
-        this.messageColor = "red";
+        this.messageColor = 'red';
       }
     } catch (error) {
       console.error(error);
     } finally {
-      setTimeout(() => this.message = "", 3000);
+      setTimeout(() => (this.message = ''), 3000);
     }
   }
 }
