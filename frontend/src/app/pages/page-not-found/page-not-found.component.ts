4b92f47bdc229bda8e3f0e9d28947f2897200262
Danny Cowles <dannycowless@gmail.com>
2025-09-14 16:01:50 -0700

    Implement 404 page via design

--- frontend/src/app/pages/page-not-found/page-not-found.component.ts
+++ frontend/src/app/pages/page-not-found/page-not-found.component.ts
@@ -1,13 +1,13 @@
 import {Component, OnInit} from '@angular/core';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-page-not-found',
   templateUrl: './page-not-found.component.html',
   styleUrl: './page-not-found.component.css',
-  standalone: true
+  imports: [RouterLink],
+  standalone: true,
 })
 export class PageNotFoundComponent implements OnInit {
-
-  ngOnInit() { }
-
+  ngOnInit() {}
 }
